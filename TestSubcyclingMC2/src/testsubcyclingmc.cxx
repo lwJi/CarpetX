ba8cac541e4a5d1c7d8eb643ef334dc9925c5a53
Liwei Ji <jiliwei.phys@gmail.com>
2025-05-02 11:05:25 -0400

    TestSubcyclingMC2: use Derivs to compute derivatives

--- TestSubcyclingMC2/src/testsubcyclingmc.cxx
+++ TestSubcyclingMC2/src/testsubcyclingmc.cxx
@@ -1,7 +1,10 @@
 #include <loop_device.hxx>
 #include <subcycling.hxx>
 
+#include <derivs.hxx>
+#include <mat.hxx>
 #include <sum.hxx>
+#include <vec.hxx>
 #include <vect.hxx>
 
 #include <cctk.h>
@@ -15,6 +18,7 @@
 
 namespace TestSubcyclingMC2 {
 using namespace Arith;
+using namespace Loop;
 
 constexpr int dim = 3;
 
@@ -64,7 +68,7 @@ extern "C" void TestSubcyclingMC2_Initial(CCTK_ARGUMENTS) {
   if (CCTK_EQUALS(initial_condition, "standing wave")) {
     grid.loop_int_device<0, 0, 0>(
         grid.nghostzones,
-        [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+        [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
           standing_wave(amplitude, standing_wave_kx, standing_wave_ky,
                         standing_wave_kz, cctk_time, p.x, p.y, p.z, u(p.I),
                         rho(p.I));
@@ -73,7 +77,7 @@ extern "C" void TestSubcyclingMC2_Initial(CCTK_ARGUMENTS) {
   } else if (CCTK_EQUALS(initial_condition, "Gaussian")) {
     grid.loop_int_device<0, 0, 0>(
         grid.nghostzones,
-        [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+        [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
           gaussian(amplitude, gaussian_width, cctk_time, p.x, p.y, p.z, u(p.I),
                    rho(p.I));
         });
@@ -87,20 +91,56 @@ extern "C" void TestSubcyclingMC2_RHS(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTSX_TestSubcyclingMC2_RHS;
   DECLARE_CCTK_PARAMETERS;
 
+  const vect<CCTK_REAL, dim> dx{
+      CCTK_DELTA_SPACE(0),
+      CCTK_DELTA_SPACE(1),
+      CCTK_DELTA_SPACE(2),
+  };
+
+  const array<int, dim> indextype = {0, 0, 0};
+  const array<int, dim> nghostzones = {cctk_nghostzones[0], cctk_nghostzones[1],
+                                       cctk_nghostzones[2]};
+  vect<int, dim> imin, imax;
+  GridDescBase(cctkGH).box_int<0, 0, 0>(nghostzones, imin, imax);
+  // suffix 2: with ghost zones, suffix 5: without ghost zones
+  const GF3D2layout layout2(cctkGH, indextype);
+  const GF3D5layout layout5(imin, imax);
+
+  // Tile variables for derivatives and so on
+  const int ntmps = 10;
+  GF3D5vector<CCTK_REAL> tmps(layout5, ntmps);
+  int itmp = 0;
+
+  const auto make_gf = [&]() { return GF3D5<CCTK_REAL>(tmps(itmp++)); };
+  const auto make_vec = [&](const auto &f) {
+    return vec<result_of_t<decltype(f)()>, 3>([&](int) { return f(); });
+  };
+  const auto make_mat = [&](const auto &f) {
+    return smat<result_of_t<decltype(f)()>, 3>([&](int, int) { return f(); });
+  };
+  const auto make_vec_gf = [&]() { return make_vec(make_gf); };
+  const auto make_mat_gf = [&]() { return make_mat(make_gf); };
+
+  const GF3D5<CCTK_REAL> tl_u(make_gf());
+  const vec<GF3D5<CCTK_REAL>, 3> tl_du(make_vec_gf());
+  const smat<GF3D5<CCTK_REAL>, 3> tl_ddu(make_mat_gf());
+
+  const int deriv_order = 4;
+  Derivs::calc_derivs2<0, 0, 0>(tl_u, tl_du, tl_ddu, layout5, grid, u, dx,
+                                deriv_order);
+
+  if (itmp != ntmps)
+    CCTK_VERROR("Wrong number of temporary variables: ntmps=%d itmp=%d", ntmps,
+                itmp);
+  itmp = -1;
+
   grid.loop_int_device<0, 0, 0>(
       grid.nghostzones,
-      [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
-        using std::pow;
-        Arith::vect<CCTK_REAL, dim> ddu;
-        for (int d = 0; d < dim; ++d) {
-          // ddu[d] = (u(p.I - p.DI[d]) - 2 * u(p.I) + u(p.I + p.DI[d])) /
-          //          pow(p.DX[d], 2);
-          ddu[d] = (-(u(p.I - 2 * p.DI[d]) + u(p.I + 2 * p.DI[d])) +
-                    16 * (u(p.I - p.DI[d]) + u(p.I + p.DI[d])) - 30 * u(p.I)) /
-                   (12 * pow(p.DX[d], 2));
-        }
+      [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+        const GF3D5index index5(layout5, p.I);
         u_rhs(p.I) = rho(p.I);
-        rho_rhs(p.I) = ddu[0] + ddu[1] + ddu[2];
+        rho_rhs(p.I) =
+            tl_ddu(0, 0)(index5) + tl_ddu(1, 1)(index5) + tl_ddu(2, 2)(index5);
       });
 }
 
@@ -116,7 +156,7 @@ extern "C" void TestSubcyclingMC2_Error(CCTK_ARGUMENTS) {
 
     grid.loop_int_device<0, 0, 0>(
         grid.nghostzones,
-        [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+        [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
           CCTK_REAL u0, rho0;
           standing_wave(amplitude, standing_wave_kx, standing_wave_ky,
                         standing_wave_kz, cctk_time, p.x, p.y, p.z, u0, rho0);
@@ -128,7 +168,7 @@ extern "C" void TestSubcyclingMC2_Error(CCTK_ARGUMENTS) {
 
     grid.loop_int_device<0, 0, 0>(
         grid.nghostzones,
-        [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+        [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
           CCTK_REAL u0, rho0;
           gaussian(amplitude, gaussian_width, cctk_time, p.x, p.y, p.z, u0,
                    rho0);
