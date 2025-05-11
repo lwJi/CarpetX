1c1b7b73d6444856e7f34967f9dd46e5c122cf48
Liwei Ji <jiliwei.phys@gmail.com>
2025-05-11 12:20:47 -0400

    Subcycling: beautify get_isrmbndry_idx

--- Subcycling/src/utils.hxx
+++ Subcycling/src/utils.hxx
@@ -4,6 +4,8 @@
 #include <cctk.h>
 #include <util_Table.h>
 
+#include <unordered_map>
+
 namespace Subcycling {
 using namespace std;
 
@@ -16,20 +18,24 @@ constexpr array<array<int, Loop::dim>, static_cast<size_t>(centering_t::ntypes)>
         array<int, Loop::dim>{1, 0, 0}, array<int, Loop::dim>{0, 1, 0},
         array<int, Loop::dim>{0, 0, 1}, array<int, Loop::dim>{0, 0, 0}};
 
+constexpr std::array<const char *, static_cast<size_t>(centering_t::ntypes)>
+    g_isrmbndry_strs = {
+        "Subcycling::isrmbndry_ccc", "Subcycling::isrmbndry_vcc",
+        "Subcycling::isrmbndry_cvc", "Subcycling::isrmbndry_ccv",
+        "Subcycling::isrmbndry_cvv", "Subcycling::isrmbndry_vcv",
+        "Subcycling::isrmbndry_vvc", "Subcycling::isrmbndry_vvv"};
+
 /**
  * \brief return refinement boundary flag grid function indexes
  */
-inline array<int, static_cast<size_t>(centering_t::ntypes)>
+
+inline std::array<int, static_cast<size_t>(centering_t::ntypes)>
 get_isrmbndry_idx() {
-  return array<int, static_cast<size_t>(centering_t::ntypes)>{
-      CCTK_VarIndex("Subcycling::isrmbndry_ccc"),
-      CCTK_VarIndex("Subcycling::isrmbndry_vcc"),
-      CCTK_VarIndex("Subcycling::isrmbndry_cvc"),
-      CCTK_VarIndex("Subcycling::isrmbndry_ccv"),
-      CCTK_VarIndex("Subcycling::isrmbndry_cvv"),
-      CCTK_VarIndex("Subcycling::isrmbndry_vcv"),
-      CCTK_VarIndex("Subcycling::isrmbndry_vvc"),
-      CCTK_VarIndex("Subcycling::isrmbndry_vvv")};
+  std::array<int, static_cast<size_t>(centering_t::ntypes)> indices;
+  for (size_t i = 0; i < g_isrmbndry_strs.size(); ++i) {
+    indices[i] = CCTK_VarIndex(g_isrmbndry_strs[i]);
+  }
+  return indices;
 }
 
 } // namespace Subcycling
