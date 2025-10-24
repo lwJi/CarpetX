c57d386f19ab3ddedb65aeff6262b0d001626b12
Allen Wen <acw6923@g.rit.edu>
2025-10-24 13:46:44 -0500

    Derivs: remove extra templates formerly used for EFL

--- CarpetX/src/driver.cxx
+++ CarpetX/src/driver.cxx
@@ -2539,28 +2539,6 @@ operator<<(YAML::Emitter &yaml,
 
 YAML::Emitter &
 operator<<(YAML::Emitter &yaml,
-           const GHExt::GlobalData::AnyTypeVector &anytypevector) {
-  yaml << YAML::BeginSeq;
-  for (size_t i = 0; i < anytypevector.size(); ++i) {
-    switch (anytypevector.type()) {
-    case CCTK_VARIABLE_REAL:
-      yaml << *(CCTK_REAL *)anytypevector.data_at(i);
-      break;
-    case CCTK_VARIABLE_INT:
-      yaml << *(CCTK_INT *)anytypevector.data_at(i);
-      break;
-    default:
-      // missed to implement a type
-      CCTK_VERROR("Cannot handle type %d", anytypevector.type());
-      break;
-    }
-  }
-  yaml << YAML::EndSeq;
-  return yaml;
-}
-
-YAML::Emitter &
-operator<<(YAML::Emitter &yaml,
            const GHExt::GlobalData::ArrayGroupData &arraygroupdata) {
   yaml << YAML::LocalTag("arraygroupdata-1.0.0");
   yaml << YAML::BeginMap;
