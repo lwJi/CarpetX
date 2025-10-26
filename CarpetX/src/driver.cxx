673aab940b18ab70b4c8072d06ac7f086d619910
Allen Wen <acw6923@g.rit.edu>
2025-10-26 13:10:00 -0500

    CarpetX: undo formatting in driver.cxx

--- CarpetX/src/driver.cxx
+++ CarpetX/src/driver.cxx
@@ -1450,6 +1450,7 @@ amrex::Interpolater *get_interpolator(const std::array<int, dim> indextype) {
         return &prolongate_poly_eno3lfb_3d_rf2_c111_o5;
       }
       break;
+
     }
     break;
 
@@ -2084,9 +2085,9 @@ void CactusAmrCore::MakeNewLevelFromCoarse(
   // in time
   if (leveldata.iteration != coarseleveldata.iteration) {
     ostringstream msg;
-    msg << "Coarse (rl=" << (level - 1) << ", it=" << coarseleveldata.iteration
-        << ") and fine (rl=" << level << ", it=" << leveldata.iteration
-        << ") grid do not align in time when regridding";
+    msg << "Coarse (rl=" << (level-1) <<", it=" << coarseleveldata.iteration <<
+           ") and fine (rl=" << level << ", it=" << leveldata.iteration <<
+           ") grid do not align in time when regridding";
 #pragma omp critical
     CCTK_VERROR(msg.str().c_str());
   }
@@ -2190,9 +2191,9 @@ void CactusAmrCore::RemakeLevel(const int level, const amrex::Real time,
   // time
   if (leveldata.iteration != coarseleveldata.iteration) {
     ostringstream msg;
-    msg << "Coarse (rl=" << (level - 1) << ", it=" << coarseleveldata.iteration
-        << ") and fine (rl=" << level << ", it=" << leveldata.iteration
-        << ") grid do not align in time when regridding";
+    msg << "Coarse (rl=" << (level-1) <<", it=" << coarseleveldata.iteration <<
+           ") and fine (rl=" << level << ", it=" << leveldata.iteration <<
+           ") grid do not align in time when regridding";
 #pragma omp critical
     CCTK_VERROR(msg.str().c_str());
   }
