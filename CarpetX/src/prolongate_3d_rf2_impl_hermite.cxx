4b03e274071c18fe09a357df4c4b22b585b93012
Liwei Ji <jiliwei.phys@gmail.com>
2025-11-19 08:41:40 -0500

    CarpetX: fix hermite prolongation

--- CarpetX/src/prolongate_3d_rf2_impl_hermite.cxx
+++ CarpetX/src/prolongate_3d_rf2_impl_hermite.cxx
@@ -4,42 +4,6 @@ namespace CarpetX {
 
 // Hermite interpolation
 
-static prolongate_3d_rf2<VC, VC, VC, HERMITE, HERMITE, HERMITE, 1, 1, 1,
-                         FB_NONE>
-    prolongate_hermite_3d_rf2_c000_o1;
-static prolongate_3d_rf2<VC, VC, CC, HERMITE, HERMITE, CONS, 1, 1, 1, FB_NONE>
-    prolongate_hermite_3d_rf2_c001_o1;
-static prolongate_3d_rf2<VC, CC, VC, HERMITE, CONS, HERMITE, 1, 1, 1, FB_NONE>
-    prolongate_hermite_3d_rf2_c010_o1;
-static prolongate_3d_rf2<VC, CC, CC, HERMITE, CONS, CONS, 1, 1, 1, FB_NONE>
-    prolongate_hermite_3d_rf2_c011_o1;
-static prolongate_3d_rf2<CC, VC, VC, CONS, HERMITE, HERMITE, 1, 1, 1, FB_NONE>
-    prolongate_hermite_3d_rf2_c100_o1;
-static prolongate_3d_rf2<CC, VC, CC, CONS, HERMITE, CONS, 1, 1, 1, FB_NONE>
-    prolongate_hermite_3d_rf2_c101_o1;
-static prolongate_3d_rf2<CC, CC, VC, CONS, CONS, HERMITE, 1, 1, 1, FB_NONE>
-    prolongate_hermite_3d_rf2_c110_o1;
-static prolongate_3d_rf2<CC, CC, CC, CONS, CONS, CONS, 1, 1, 1, FB_NONE>
-    prolongate_hermite_3d_rf2_c111_o1;
-
-static prolongate_3d_rf2<VC, VC, VC, HERMITE, HERMITE, HERMITE, 3, 3, 3,
-                         FB_NONE>
-    prolongate_hermite_3d_rf2_c000_o3;
-static prolongate_3d_rf2<VC, VC, CC, HERMITE, HERMITE, CONS, 3, 3, 3, FB_NONE>
-    prolongate_hermite_3d_rf2_c001_o3;
-static prolongate_3d_rf2<VC, CC, VC, HERMITE, CONS, HERMITE, 3, 3, 3, FB_NONE>
-    prolongate_hermite_3d_rf2_c010_o3;
-static prolongate_3d_rf2<VC, CC, CC, HERMITE, CONS, CONS, 3, 3, 3, FB_NONE>
-    prolongate_hermite_3d_rf2_c011_o3;
-static prolongate_3d_rf2<CC, VC, VC, CONS, HERMITE, HERMITE, 3, 3, 3, FB_NONE>
-    prolongate_hermite_3d_rf2_c100_o3;
-static prolongate_3d_rf2<CC, VC, CC, CONS, HERMITE, CONS, 3, 3, 3, FB_NONE>
-    prolongate_hermite_3d_rf2_c101_o3;
-static prolongate_3d_rf2<CC, CC, VC, CONS, CONS, HERMITE, 3, 3, 3, FB_NONE>
-    prolongate_hermite_3d_rf2_c110_o3;
-static prolongate_3d_rf2<CC, CC, CC, CONS, CONS, CONS, 3, 3, 3, FB_NONE>
-    prolongate_hermite_3d_rf2_c111_o3;
-
 static prolongate_3d_rf2<VC, VC, VC, HERMITE, HERMITE, HERMITE, 5, 5, 5,
                          FB_NONE>
     prolongate_hermite_3d_rf2_c000_o5;
@@ -58,30 +22,44 @@ static prolongate_3d_rf2<CC, CC, VC, CONS, CONS, HERMITE, 5, 5, 5, FB_NONE>
 static prolongate_3d_rf2<CC, CC, CC, CONS, CONS, CONS, 5, 5, 5, FB_NONE>
     prolongate_hermite_3d_rf2_c111_o5;
 
+static prolongate_3d_rf2<VC, VC, VC, HERMITE, HERMITE, HERMITE, 7, 7, 7,
+                         FB_NONE>
+    prolongate_hermite_3d_rf2_c000_o7;
+static prolongate_3d_rf2<VC, VC, CC, HERMITE, HERMITE, CONS, 7, 7, 7, FB_NONE>
+    prolongate_hermite_3d_rf2_c001_o7;
+static prolongate_3d_rf2<VC, CC, VC, HERMITE, CONS, HERMITE, 7, 7, 7, FB_NONE>
+    prolongate_hermite_3d_rf2_c010_o7;
+static prolongate_3d_rf2<VC, CC, CC, HERMITE, CONS, CONS, 7, 7, 7, FB_NONE>
+    prolongate_hermite_3d_rf2_c011_o7;
+static prolongate_3d_rf2<CC, VC, VC, CONS, HERMITE, HERMITE, 7, 7, 7, FB_NONE>
+    prolongate_hermite_3d_rf2_c100_o7;
+static prolongate_3d_rf2<CC, VC, CC, CONS, HERMITE, CONS, 7, 7, 7, FB_NONE>
+    prolongate_hermite_3d_rf2_c101_o7;
+static prolongate_3d_rf2<CC, CC, VC, CONS, CONS, HERMITE, 7, 7, 7, FB_NONE>
+    prolongate_hermite_3d_rf2_c110_o7;
+static prolongate_3d_rf2<CC, CC, CC, CONS, CONS, CONS, 7, 7, 7, FB_NONE>
+    prolongate_hermite_3d_rf2_c111_o7;
+
+static prolongate_3d_rf2<VC, VC, VC, HERMITE, HERMITE, HERMITE, 9, 9, 9,
+                         FB_NONE>
+    prolongate_hermite_3d_rf2_c000_o9;
+static prolongate_3d_rf2<VC, VC, CC, HERMITE, HERMITE, CONS, 9, 9, 9, FB_NONE>
+    prolongate_hermite_3d_rf2_c001_o9;
+static prolongate_3d_rf2<VC, CC, VC, HERMITE, CONS, HERMITE, 9, 9, 9, FB_NONE>
+    prolongate_hermite_3d_rf2_c010_o9;
+static prolongate_3d_rf2<VC, CC, CC, HERMITE, CONS, CONS, 9, 9, 9, FB_NONE>
+    prolongate_hermite_3d_rf2_c011_o9;
+static prolongate_3d_rf2<CC, VC, VC, CONS, HERMITE, HERMITE, 9, 9, 9, FB_NONE>
+    prolongate_hermite_3d_rf2_c100_o9;
+static prolongate_3d_rf2<CC, VC, CC, CONS, HERMITE, CONS, 9, 9, 9, FB_NONE>
+    prolongate_hermite_3d_rf2_c101_o9;
+static prolongate_3d_rf2<CC, CC, VC, CONS, CONS, HERMITE, 9, 9, 9, FB_NONE>
+    prolongate_hermite_3d_rf2_c110_o9;
+static prolongate_3d_rf2<CC, CC, CC, CONS, CONS, CONS, 9, 9, 9, FB_NONE>
+    prolongate_hermite_3d_rf2_c111_o9;
+
 const std::map<int, std::array<amrex::Interpolater *, 8> >
     prolongate_hermite_3d_rf2{
-        {1,
-         {
-             &prolongate_hermite_3d_rf2_c000_o1,
-             &prolongate_hermite_3d_rf2_c001_o1,
-             &prolongate_hermite_3d_rf2_c010_o1,
-             &prolongate_hermite_3d_rf2_c011_o1,
-             &prolongate_hermite_3d_rf2_c100_o1,
-             &prolongate_hermite_3d_rf2_c101_o1,
-             &prolongate_hermite_3d_rf2_c110_o1,
-             &prolongate_hermite_3d_rf2_c111_o1,
-         }},
-        {3,
-         {
-             &prolongate_hermite_3d_rf2_c000_o3,
-             &prolongate_hermite_3d_rf2_c001_o3,
-             &prolongate_hermite_3d_rf2_c010_o3,
-             &prolongate_hermite_3d_rf2_c011_o3,
-             &prolongate_hermite_3d_rf2_c100_o3,
-             &prolongate_hermite_3d_rf2_c101_o3,
-             &prolongate_hermite_3d_rf2_c110_o3,
-             &prolongate_hermite_3d_rf2_c111_o3,
-         }},
         {5,
          {
              &prolongate_hermite_3d_rf2_c000_o5,
@@ -92,6 +70,28 @@ const std::map<int, std::array<amrex::Interpolater *, 8> >
              &prolongate_hermite_3d_rf2_c101_o5,
              &prolongate_hermite_3d_rf2_c110_o5,
              &prolongate_hermite_3d_rf2_c111_o5,
+         }},
+        {7,
+         {
+             &prolongate_hermite_3d_rf2_c000_o7,
+             &prolongate_hermite_3d_rf2_c001_o7,
+             &prolongate_hermite_3d_rf2_c010_o7,
+             &prolongate_hermite_3d_rf2_c011_o7,
+             &prolongate_hermite_3d_rf2_c100_o7,
+             &prolongate_hermite_3d_rf2_c101_o7,
+             &prolongate_hermite_3d_rf2_c110_o7,
+             &prolongate_hermite_3d_rf2_c111_o7,
+         }},
+        {9,
+         {
+             &prolongate_hermite_3d_rf2_c000_o9,
+             &prolongate_hermite_3d_rf2_c001_o9,
+             &prolongate_hermite_3d_rf2_c010_o9,
+             &prolongate_hermite_3d_rf2_c011_o9,
+             &prolongate_hermite_3d_rf2_c100_o9,
+             &prolongate_hermite_3d_rf2_c101_o9,
+             &prolongate_hermite_3d_rf2_c110_o9,
+             &prolongate_hermite_3d_rf2_c111_o9,
          }},
     };
 
